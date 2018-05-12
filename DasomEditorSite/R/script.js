a37a3100c09015ce25f1fc58c2234f08a6570312
YJ <hanul@hanul.me>
2018-05-13 01:14:12 +0900

    닫기 기능 추가

--- DasomEditorSite/R/script.js
+++ DasomEditorSite/R/script.js
@@ -1,1 +1,24 @@
-console.log('test');
+let isControlMode;
+
+EVENT('keydown', (e) => {
+	
+	if (isControlMode === true) {
+		
+		let key = e.getKey().toLowerCase();
+		
+		// 현재 탭 종료
+		if (key === 'w') {
+			location.href = 'about:blank';
+		}
+	}
+	
+	if (e.getKey() === 'Control') {
+		isControlMode = true;
+	}
+});
+
+EVENT('keyup', (e) => {
+	if (e.getKey() === 'Control') {
+		isControlMode = false;
+	}
+});
